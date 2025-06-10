2df24c9c1629715c3886651ca7e79ca658e97ab2
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-06-10 12:26:14 +0800

    745

--- 745/solution.py
+++ 745/solution.py
@@ -15,19 +15,66 @@ from typing import List, Optional
 #         return res
 
 
+# # hash map, time: init: O(n*m^3), f: O(m), space: O(n*m^3), TLE
+# class WordFilter:
+#     def __init__(self, words: List[str]) -> None:
+#         self.mp = {}
+#         for i, word in enumerate(words):
+#             for j in range(1, len(word) + 1):
+#                 pref = word[:j]
+#                 for k in range(len(word)):
+#                     suff = word[k:]
+#                     self.mp[pref + "$" + suff] = i
+#
+#     def f(self, pref: str, suff: str) -> int:
+#         word = pref + "$" + suff
+#         return self.mp[word] if word in self.mp else -1
+
+
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = [None] * 27
+        self.index = -1
+
+
+# # trie, time: init: O(n*m^3), f: O(m), space: O(n*m^3)
+class Trie:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+        pass
+
+    def insert(self, word: str, index: int) -> None:
+        curr = self.root
+        for ch in word:
+            char_index = ord(ch) - ord("a")
+            if not curr.children[char_index]:
+                curr.children[char_index] = TrieNode()
+            curr = curr.children[char_index]
+        curr.index = index
+
+    def search(self, word) -> int:
+        curr = self.root
+        for ch in word:
+            char_index = ord(ch) - ord("a")
+            if not curr.children[char_index]:
+                return -1
+            curr = curr.children[char_index]
+        return curr.index
+
+
 class WordFilter:
     def __init__(self, words: List[str]) -> None:
-        self.mp = {}
+        self.trie = Trie()
         for i, word in enumerate(words):
             for j in range(1, len(word) + 1):
                 pref = word[:j]
                 for k in range(len(word)):
                     suff = word[k:]
-                    self.mp[pref + "}" + suff] = i
+                    self.trie.insert(pref + "{" + suff, i)
 
     def f(self, pref: str, suff: str) -> int:
-        word = pref + "}" + suff
-        return self.mp[word] if word in self.mp else -1
+        word = pref + "{" + suff
+        return self.trie.search(word)
 
 
 class Test(unittest.TestCase):
